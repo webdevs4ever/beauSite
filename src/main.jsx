e84fa975878171750026d9be553d36c685ffc0d2
RTF <rasheim.freeman@publicissapient.com>
2026-09-08 13:54:41 -0400

    added scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import ScrollToTop from './components/ScrollToTop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
